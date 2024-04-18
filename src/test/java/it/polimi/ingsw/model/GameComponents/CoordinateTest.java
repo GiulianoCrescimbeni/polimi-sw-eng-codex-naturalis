62c4b91beeca8c8ea40719b822365beae1d10d71
mauro <mauro.leporace@mail.polimi.it>
2024-04-18 19:06:33 +0200

    -Added CoordinateTest

---
+++ src/test/java/it/polimi/ingsw/model/GameComponents/CoordinateTest.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.model.GameComponents;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class CoordinateTest extends TestCase {
+
+    int x1 = 10;
+    int y1 = 10;
+    int x2 = 20;
+    int y2 = 20;
+
+    Coordinate toTest = new Coordinate(x1, y1);
+    Coordinate other = new Coordinate(x2, y2);
+
+    @Test
+    public void testGetX() {
+        assertEquals(x1, toTest.getX());
+    }
+
+    @Test
+    public void testGetY() {
+        assertEquals(y1, toTest.getY());
+    }
+
+    @Test
+    public void testEquals() {
+        assertFalse(toTest.equals(other));
+    }
+}
