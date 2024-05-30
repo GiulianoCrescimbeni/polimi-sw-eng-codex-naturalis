bd44010fcfff820f1eb55ede8c372f1f9968b0cb
giulianocrescimbeni <giuliano.crescimbeni@mail.polimi.it>
2024-05-30 11:34:49 +0200

    module-info fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,17 @@ module it.polimi.ingsw {
     requires javafx.graphics;
     requires java.rmi;
     requires com.google.gson;
+    exports it.polimi.ingsw.network.server.RMI;
+    exports it.polimi.ingsw.network.server.handler;
+    exports it.polimi.ingsw.model.Data;
+    exports it.polimi.ingsw.model.Enumerations;
+    exports it.polimi.ingsw.model.GameComponents;
     exports it.polimi.ingsw.view.GUI;
+    opens it.polimi.ingsw.network.server.RMI to java.rmi;
+    opens it.polimi.ingsw.network.server.handler to java.rmi;
+    opens it.polimi.ingsw.model.Data to com.google.gson;
+    opens it.polimi.ingsw.model.Enumerations to com.google.gson;
+    opens it.polimi.ingsw.model.GameComponents to com.google.gson;
     opens it.polimi.ingsw.view.GUI to javafx.graphics;
     opens it.polimi.ingsw to javafx.fxml;
 }
