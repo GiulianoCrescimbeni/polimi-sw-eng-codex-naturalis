9bfa5ab81178e75b6b747e551fc93ee884868a6c
giulianocrescimbeni <giuliano.crescimbeni@mail.polimi.it>
2024-06-17 02:31:25 +0200

    Codex GUI Update

--- src/main/java/it/polimi/ingsw/view/GUI/controllers/GameController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/controllers/GameController.java
@@ -1,14 +1,19 @@
 package it.polimi.ingsw.view.GUI.controllers;
 
+import it.polimi.ingsw.model.Enumerations.Color;
+import it.polimi.ingsw.model.GameComponents.Coordinate;
+import it.polimi.ingsw.network.client.ClientController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -35,7 +40,40 @@ public class GameController {
     private ImageView background;
 
     @FXML
-    private ImageView centre;
+    private ImageView InitialCard;
+
+    @FXML
+    private ImageView Hand1;
+
+    @FXML
+    private ImageView Hand2;
+
+    @FXML
+    private ImageView Hand3;
+
+    @FXML
+    private ImageView GroundCard1;
+
+    @FXML
+    private ImageView GroundCard2;
+
+    @FXML
+    private ImageView GroundGoldCard1;
+
+    @FXML
+    private ImageView GroundGoldCard2;
+
+    @FXML
+    private ImageView CommonGoal1;
+
+    @FXML
+    private ImageView CommonGoal2;
+
+    @FXML
+    private ImageView PersonalGoal;
+
+    @FXML
+    private Text CurrentPlayerNameText;
 
     @FXML
     private Button plateauButton;
@@ -112,8 +150,6 @@ public class GameController {
         img10.setLayoutY(img10.getLayoutY() - anchor.getLayoutY());
     }
 
-
-
     private double minX;
     private double maxX;
     private double minY;
@@ -123,6 +159,18 @@ public class GameController {
     public void initialize() {
         updateLimits();
         background.imageProperty().addListener((observable, oldValue, newValue) -> updateLimits());
+        setCardImage(PersonalGoal, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getCodexMap().get(ClientController.getInstance().getPlayerByUsername(ClientController.getInstance().getUsername())).getCard(new Coordinate(80, 80)).getCardID() +".png");
+        setCardImage(Hand1, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getPlayerHand().getCards().get(0).getCardID() +".png");
+        setCardImage(Hand2, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getPlayerHand().getCards().get(1).getCardID() +".png");
+        setCardImage(Hand3, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getPlayerHand().getCards().get(2).getCardID() +".png");
+        setCardImage(GroundCard1, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getCardToPick().get(0).getCardID() +".png");
+        setCardImage(GroundCard2, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getCardToPick().get(1).getCardID() +".png");
+        setCardImage(GroundGoldCard1, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getGoldCardToPick().get(0).getCardID() +".png");
+        setCardImage(GroundGoldCard2, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getGoldCardToPick().get(1).getCardID() +".png");
+        setCardImage(CommonGoal1, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getCommonGoals().get(0).getGoalId() +".png");
+        setCardImage(CommonGoal2, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getCommonGoals().get(1).getGoalId() +".png");
+        setCardImage(PersonalGoal, "/polimi/ingsw/Cards/Fronts/"+ ClientController.getInstance().getPersonalGoal().getGoalId() +".png");
+        printCurrentPlayer();
     }
     private void updateLimits() {
 
@@ -147,19 +195,43 @@ public class GameController {
         // Verifico che le nuove coordinate siano nei limiti
         if (newLayoutX >= minX && newLayoutX <= maxX) {
             background.setLayoutX(newLayoutX);
-            centre.setLayoutX(newLayoutX);
+            InitialCard.setLayoutX(newLayoutX);
         }
 
         if (newLayoutY >= minY && newLayoutY <= maxY) {
             background.setLayoutY(newLayoutY);
-            centre.setLayoutY(newLayoutY);
+            InitialCard.setLayoutY(newLayoutY);
         }
 
         mouseX = event.getSceneX();
         mouseY = event.getSceneY();
     }
 
+    private void setCardImage(ImageView imageView, String imagePath) {
+        try {
+            Image image = new Image(getClass().getResource(imagePath).toExternalForm());
+            imageView.setImage(image);
+        } catch (NullPointerException e) {
+            System.out.println("Image not found: " + imagePath);
+        }
+    }
 
-
+    private void printCurrentPlayer() {
+        CurrentPlayerNameText.setText(ClientController.getInstance().getCurrentPlayer().getNickname());
+        switch (ClientController.getInstance().getCurrentPlayer().getColor()) {
+            case Color.Blue:
+                CurrentPlayerNameText.setFill(javafx.scene.paint.Color.BLUE);
+                break;
+            case Color.Red:
+                CurrentPlayerNameText.setFill(javafx.scene.paint.Color.RED);
+                break;
+            case Color.Yellow:
+                CurrentPlayerNameText.setFill(javafx.scene.paint.Color.web("#edb009"));
+                break;
+            case Color.Green:
+                CurrentPlayerNameText.setFill(javafx.scene.paint.Color.GREEN);
+                break;
+        }
+    }
 
 }
